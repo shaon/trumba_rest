8a976969ca845e641346f7b3c3259812f1b2b97a
Imran Hossain Shaon <mdshaonimran@gmail.com>
2016-03-19 16:36:06 -0700

    use requests

--- trumba_rest/__init__.py
+++ trumba_rest/__init__.py
@@ -1,4 +1,4 @@
-import urllib
+import requests
 from flask import Flask
 from flask_restful import Resource, Api
 from icalendar import Calendar
@@ -12,19 +12,23 @@ class HelloPark(Resource):
 
     def get(self, calendar_name):
         global_dict = {"events": []}
-
+        
         target_url = "http://www.trumba.com/calendars/" + calendar_name + ".ics"
-        txt = urllib.urlopen(target_url).read()
-        cal = Calendar.from_ical(txt)
-        events = global_dict["events"]
-        for event in cal.walk('vevent'):
-            keydict = {}
-            for key in event.keys():
-                if (key == 'DTSTART') or (key == 'DTEND') or (key == 'DTSTAMP'):
-                    keydict[key.lower()] = event.get(key).dt.isoformat()
-                else:
-                    keydict[key.lower()] = event.get(key)
-            events.append(keydict)
-        return global_dict
+        try:
+            r = requests.get(target_url)
+            r.close()
+            cal = Calendar.from_ical(r.content)
+            events = global_dict["events"]
+            for event in cal.walk('vevent'):
+                keydict = {}
+                for key in event.keys():
+                    if (key == 'DTSTART') or (key == 'DTEND') or (key == 'DTSTAMP'):
+                        keydict[key.lower()] = event.get(key).dt.isoformat()
+                    else:
+                        keydict[key.lower()] = event.get(key)
+                events.append(keydict)
+            return global_dict
+        except:
+            print "ERROR: Reading file."
 
 api.add_resource(HelloPark, '/calendar/<string:calendar_name>')
